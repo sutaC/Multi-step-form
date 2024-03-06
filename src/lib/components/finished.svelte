c5ace92924c4a7f5127010fa6f610f1a6c57b59c
Catus <catus.pl@gmail.com>
2024-03-06 11:21:28 +0100

    Summary

--- src/lib/components/finished.svelte
+++ src/lib/components/finished.svelte
@@ -1,1 +1,34 @@
-<h1>Finished</h1>
+<div class="finished">
+	<i class="icon checkmark"></i>
+	<h1>Thank you!</h1>
+	<p>
+		Thanks for confirming your subscription! We hope you have fun using our platform. If you ever
+		need support, please feel free to email us at support@loremgaming.com.
+	</p>
+</div>
+
+<style>
+	.finished {
+		text-align: center;
+		padding: 3rem 0.75em;
+	}
+
+	p {
+		color: var(--clr-text-shaded);
+	}
+
+	.icon {
+		display: block;
+		width: 2rem;
+		aspect-ratio: 1;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: contain;
+	}
+
+	.icon.checkmark {
+		background-image: url('icon-thank-you.svg');
+		width: 3rem;
+		margin: 0 auto 1.5rem;
+	}
+</style>
